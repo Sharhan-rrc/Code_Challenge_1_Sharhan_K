62d98f28d935f5b2dac5fac26c1d057cedfb305b
Sharhan Kaji <shabibbhaikaji@rrc.ca>
2026-01-20 17:56:34 -0600

    feat: add sample player data to playerService

--- src/services/playerService.ts
+++ src/services/playerService.ts
@@ -14,3 +14,28 @@ export interface RatingResponse {
   rating: number;
   gamesPlayed: number;
 }
+
+// Sample data representing a list of players that was given in instructions
+const players: Player[] = [
+  {
+    id: 1,
+    name: "ShadowStrike",
+    wins: 15,
+    losses: 5,
+    totalScore: 28500
+  },
+  {
+    id: 2,
+    name: "NoobMaster",
+    wins: 3,
+    losses: 12,
+    totalScore: 4200
+  },
+  {
+    id: 3,
+    name: "ProGamer99",
+    wins: 0,
+    losses: 0,
+    totalScore: 0
+  }
+];
